330dc85576b76d6f1a836430084064516f307eac
TechSolute <pharshiesammy@gmail.com>
2026-08-25 02:35:43 +0100

    feat(phase3): load technical analysis styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./phase-pages.css";
+import "./technical-analysis.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
