c229e24f35893a9d230a6d8ec00eeb5e02f0dffa
TechSolute <pharshiesammy@gmail.com>
2026-09-05 20:26:26 +0100

    feat: mount AI market research tab

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AIResearchTab from "./AIResearchTab";
 import "./styles.css";
 import "./phase-pages.css";
 import "./technical-analysis.css";
+import "./ai-tab.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <AIResearchTab />
   </React.StrictMode>,
 );
